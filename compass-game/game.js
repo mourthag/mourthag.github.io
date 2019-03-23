62e09a8ffbedd273360a3b854b562ec7018205e7
Julian Spittel <julispittel@googlemail.com>
2019-03-23 12:56:55 +0100

    Use absolute orientation for compass

--- compass-game/game.js
+++ compass-game/game.js
@@ -3,7 +3,7 @@ var userPosition;
 window.onload =
     function() {
 
-        window.addEventListener("deviceorientation", handleOrientation);
+        window.addEventListener("deviceorientationabsolute", handleOrientation);
 
         $.get('https://restcountries.eu/rest/v2/name/japan?fields=name;latlng', function(data, status) {
             countries = data;
