6addccc29ab8081b3b9c48cc68550f1ce75b716c
Julian Spittel <julispittel@googlemail.com>
2019-03-25 11:40:12 +0100

    Increased tilting angle

--- compass-game/game.js
+++ compass-game/game.js
@@ -52,7 +52,7 @@ function guess() {
 function handleOrientation(event) {
     currentDirection = (360 - event.alpha);
 
-    if(Math.abs(event.beta) > 35 || Math.abs(event.gamma) > 35)
+    if(Math.abs(event.beta) > 45 || Math.abs(event.gamma) > 45)
         document.getElementById("tiltOverlay").style.display = "block";
     else
         document.getElementById("tiltOverlay").style.display = "none";
